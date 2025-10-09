0222ddd2de58bc9e5b46a8c93f5cdbb777a8c5b3
asad-technics <jahongirmutual@gmail.com>
2025-10-09 09:40:18 +0500

    fgg

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ logging.basicConfig(level=logging.INFO)  # Production uchun INFO levelga o'tkazd
 CHANNEL_USERNAME = os.environ.get('CHANNEL_USERNAME', '@havolardf')
 CONTESTS_URL = os.environ.get('CONTESTS_URL', 'https://raw.githubusercontent.com/USERNAME/REPO/main/contests.json')  # Real URL bilan almashtiring
 BOT_TOKEN = os.environ.get('BOT_TOKEN', '7897761159:AAFkmR8-RKnHD1kn2p-Va7ZSjaC5uyeBcps')
-WEBHOOK_URL = os.environ.get('WEBHOOK_URL', "127.0.0.1:443/webhook")  # Masalan: 'https://your-app-name.herokuapp.com/webhook' – deployment da o'rnating
+WEBHOOK_URL = os.environ.get('WEBHOOK_URL', "https://havolalardfbot.onrender.com/webhook")  # Masalan: 'https://your-app-name.herokuapp.com/webhook' – deployment da o'rnating
 PORT = int(os.environ.get('PORT', 443))  # Heroku uchun PORT env var
 
 if not WEBHOOK_URL:
